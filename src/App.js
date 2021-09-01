3b42610b3d621904306e7f3ef0c83ac55441350d
JeffKnowlesJr <jeffknowlesjr@gmail.com>
2021-09-01 11:54:45 -0400

    Set up class based App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React, { Component } from 'react'
 import './App.css'
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello from React</h1>
-    </div>
-  )
+// Class based component
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello from React</h1>
+      </div>
+    )
+  }
 }
 
 export default App
