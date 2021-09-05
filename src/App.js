a6bdf513d441b5331d0fcfc0564f7f115521a4de
JeffKnowlesJr <jeffknowlesjr@gmail.com>
2021-09-05 16:31:12 -0400

    Setup alert

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
 import Search from './components/users/Search'
+import Alert from './components/layout/Alert'
 import axios from 'axios'
 import './App.css'
 
@@ -9,7 +10,8 @@ import './App.css'
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   // async componentDidMount() {
@@ -46,18 +48,29 @@ class App extends Component {
     this.setState({ users: [], loading: false })
   }
 
+  // Set alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } })
+
+    setTimeout(() => {
+      this.setState({ alert: null })
+    }, 5000)
+  }
+
   render() {
     // Destructuring
-    const { users, loading } = this.state
+    const { users, loading, alert } = this.state
 
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
 
           <Users loading={loading} users={users} />
