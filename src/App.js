b6c019e8b398b82b747cb9bebcea2b5dae9ddd58
JeffKnowlesJr <jeffknowlesjr@gmail.com>
2021-09-06 13:15:19 -0400

    initialized single user page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Alert from './components/layout/Alert'
 import Users from './components/users/Users'
+import User from './components/users/User'
 import Search from './components/users/Search'
 import About from './components/pages/About'
 import axios from 'axios'
@@ -12,6 +13,7 @@ import './App.css'
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
@@ -37,8 +39,22 @@ class App extends Component {
       const res = await axios.get(
         `https://api.github.com/search/users?q=${text}&client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
       )
-      console.log('Preerror', res.data)
       this.setState({ users: res.data.items, loading: false })
+    } catch (error) {
+      console.log(error)
+      this.setState({ loading: false })
+    }
+  }
+
+  // Get single Github User
+  getUser = async (username) => {
+    this.setState({ loading: true })
+
+    try {
+      const res = await axios.get(
+        `https://api.github.com/users/${username}?client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      )
+      this.setState({ user: res.data, loading: false })
     } catch (error) {
       console.log(error)
       this.setState({ loading: false })
@@ -61,7 +77,7 @@ class App extends Component {
 
   render() {
     // Destructuring
-    const { users, loading, alert } = this.state
+    const { users, loading, alert, user } = this.state
 
     return (
       <Router>
@@ -83,6 +99,20 @@ class App extends Component {
                       setAlert={this.setAlert}
                     />
                     <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route
+                exact
+                path="/user/:login"
+                render={(props) => (
+                  <Fragment>
+                    <User
+                      {...props}
+                      getUser={this.getUser}
+                      user={user}
+                      loading={loading}
+                    />
                   </Fragment>
                 )}
               />
