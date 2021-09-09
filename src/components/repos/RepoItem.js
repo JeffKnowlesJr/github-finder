425cbea8e2d8f20b913bdcf8281085ddda999c5e
JeffKnowlesJr <jeffknowlesjr@gmail.com>
2021-09-08 21:30:52 -0400

    set up validate url function in repo item

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -1,6 +1,12 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
+const validateUrl = (value) => {
+  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
+    value
+  )
+}
+
 const RepoItem = ({ repo }) => {
   return (
     <div className="card">
@@ -14,7 +20,7 @@ const RepoItem = ({ repo }) => {
             </span>
           </Fragment>
         )}
-        {repo.homepage != null && (
+        {validateUrl(repo.homepage) && (
           <Fragment>
             <span style={{ fontSize: '.8em', fontWeight: '500' }}>{' ('}</span>
             <span style={{ fontSize: '.8em', fontWeight: '600' }}>
